f490958d6f59583bd06ba3a261ea37c56422d6b0
Xiangzhuang Shen <datahonor@gmail.com>
2025-03-06 14:59:21 +0800

    fix: test

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,19 +1,12 @@
-from hypothesis import Verbosity, given, settings
-from hypothesis.strategies import floats, integers, lists, text
+from hypothesis import given, settings
+from hypothesis.strategies import floats, lists
 
 from mppt.sort import quick_sort
 
-integer_list = lists(integers(), min_size=0, max_size=50)
 float_list = lists(floats(allow_nan=False, allow_infinity=False), min_size=0, max_size=50)
-sting_list = lists(text(), min_size=0, max_size=50)
 
 
 class TestQuickSort:
-    @given(items=integer_list)
-    @settings(max_examples=100, verbosity=Verbosity.debug)
-    def test_integer(self, items: list[int]):
-        assert quick_sort(items) == sorted(items)
-
     @given(items=float_list)
     @settings(max_examples=300)
     def test_float(self, items: list[float]):
