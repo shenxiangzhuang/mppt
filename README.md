4ea91cadaaa3ceea613ffccface1dbe9e66dcadd
Xiangzhuang Shen <datahonor@gmail.com>
2025-03-06 14:40:54 +0800

    refactor(linter&formater): all in ruff, remove flake8, isort and black

--- README.md
+++ README.md
@@ -12,9 +12,7 @@
 [![PyPI](https://img.shields.io/pypi/v/mppt?color=%2334D058&label=pypi%20package)](https://pypi.org/project/mppt/)
 [![PyPI Downloads](https://static.pepy.tech/badge/mppt)](https://pepy.tech/projects/mppt)
 
-[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 [![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
-[![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336)](https://pycqa.github.io/isort/)
 [![Checked with mypy](https://www.mypy-lang.org/static/mypy_badge.svg)](https://mypy-lang.org/)
 [![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit)](https://github.com/pre-commit/pre-commit)
 
--- mppt/sort.py
+++ mppt/sort.py
@@ -1,26 +1,23 @@
-from typing import List, Union
-
-Item = Union[float, int]
+"""Sorting algorithms."""
 
 
-def quick_sort(xs: List[Item]) -> List[Item]:
-    """
-    Quick sort: A very radical implementation
+def quick_sort(xs: list[float]) -> list[float]:
+    """Quick sort: A very radical implementation.
 
-    >>> quick_sort([1, 3, 2, 4, 5])
-    [1, 2, 3, 4, 5]
-    >>> quick_sort([1.1, 3.3, 2.2, 4.4, 5.5])
-    [1.1, 2.2, 3.3, 4.4, 5.5]
+    Args:
+        xs: comparable list
 
-    :param xs: comparable list
-    :return: sorted list
+    Returns:
+        sorted list
+
+    Examples:
+        >>> quick_sort([1, 3, 2, 4, 5])
+        [1, 2, 3, 4, 5]
+        >>> quick_sort([1.1, 3.3, 2.2, 4.4, 5.5])
+        [1.1, 2.2, 3.3, 4.4, 5.5]
     """
     return (
-        (
-            quick_sort([x for x in xs[1:] if x <= xs[0]])
-            + [xs[0]]
-            + quick_sort([x for x in xs[1:] if x > xs[0]])
-        )
+        (quick_sort([x for x in xs[1:] if x <= xs[0]]) + [xs[0]] + quick_sort([x for x in xs[1:] if x > xs[0]]))
         if len(xs) > 1
         else xs
     )
